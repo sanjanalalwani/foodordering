14ea228889afcd336ba4fadb65b458e5e6344b3c
Sanjana Lalwani <109654794+sanjanalalwani@users.noreply.github.com>
2023-03-19 11:10:37 +0530

    init

--- footer.php
+++
@@ -1,9 +0,0 @@
-
- <div class="footer">
-        <div class="wrapper">
-            <p class="text-center">2023 All rights reserved, Catering System, Developed By- <a href=""> Sanjana Lalwani</a></p>
-        </div>
-    </div>
-</body>
-
-</html>
