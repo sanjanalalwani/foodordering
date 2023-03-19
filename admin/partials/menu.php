4243f9853cfd5719b68533b6cbf4979d277ded43
Sanjana Lalwani <109654794+sanjanalalwani@users.noreply.github.com>
2023-03-19 11:26:35 +0530

    init

---
+++ admin/partials/menu.php
@@ -0,0 +1,25 @@
+<?php include('../config/constants.php');
+include('login_check.php');
+
+?>
+
+<html>
+
+<head>
+    <title>Catering order website-Home Page</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+    <!-- Menu section -->
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="manage_admin.php">Admin</a></li>
+                <li><a href="manage_category.php">Category</a></li>
+                <li><a href="manage_food.php">Food</a></li>
+                <li><a href="manage_order.php">Order</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+        </div>
